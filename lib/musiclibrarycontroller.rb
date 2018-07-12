2455a68a5e3a290e5b3bbb27312a027fbd1f0a7c
corytveten <corytveten@gmail.com>
2018-07-12 14:03:10 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -11,28 +11,29 @@ class MusicLibraryController
     input = ''
     @sorted_songs = sorter
     while input != "exit"
-    puts "What would you like to do?"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-      input = gets.strip
-      case input
-      when "list songs"
-        songs
-      when  "list artists"
-        artists
-      when "list genres"
-        genres
-      when "play song"
-        play_song
-      when "list artist"
-        list_artist
-      when "list genre"
-        list_genre
+      puts "What would you like to do?"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+        input = gets.strip
+        case input
+        when "list songs"
+          songs
+        when  "list artists"
+          artists
+        when "list genres"
+          genres
+        when "play song"
+          play_song
+        when "list artist"
+          list_artist
+        when "list genre"
+          list_genre
+        end
       end
   end
 
