285eeaa2a1273ad87e811f359a09ead149a916be
corytveten <corytveten@gmail.com>
2018-07-12 13:43:58 +0000

    automatically backed up by learn

---
+++ lib/musiclibrarycontroller.rb
@@ -0,0 +1,16 @@
+
+
+class MusicLibraryController
+
+  def initialize(path = "./db/mp3s")
+    MusicImporter.new(path).import
+  end
+
+  def call
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+
+  end
+
+end
